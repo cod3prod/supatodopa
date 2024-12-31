1bcd8b4a54578c755e65ac78fd4f7a72619a1fdc
cod3prod <cod3prod@gmail.com>
2025-01-01 01:06:47 +0900

    ✨ Implement updating user

--- src/app/(profile)/_components/update-button.tsx
+++ src/app/(profile)/_components/update-button.tsx
@@ -1,7 +1,84 @@
+"use client";
+
+import { useState } from "react";
+import { supabaseService } from "@/libs/supabase-client";
+import { supabase } from "@/libs/supabase-client";
+import { useAuthStore } from "@/zustand/auth-store";
+
 export default function UpdateButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [displayName, setDisplayName] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const { session } = useAuthStore();
+
+  const handleUpdate = async () => {
+    if (!session) {
+      console.error("User session is missing.");
+      return;
+    }
+
+    const { error } = await supabaseService.auth.admin.updateUserById(
+      session.user.id,
+      {
+        password: password || undefined,
+        user_metadata: {
+          display_name: displayName || undefined,
+        },
+      }
+    );
+
+    if (error) {
+      console.error("Failed to update user:", error.message);
+      setIsModalOpen(false);
+      return;
+    }
+
+    await supabase.auth.refreshSession();
+    console.log("User updated successfully.");
+    setIsModalOpen(false);
+  };
+
   return (
-    <button className="bg-primary text-white py-3 rounded-md shadow-md hover:bg-primary/90 w-full">
-      프로필 수정
-    </button>
+    <>
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="bg-primary text-white py-3 rounded-md shadow-md hover:bg-primary/90 w-full"
+      >
+        프로필 수정
+      </button>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="bg-white p-6 rounded-md shadow-lg max-w-sm w-full">
+            <h2 className="text-xl font-semibold mb-4">프로필 수정</h2>
+            <input
+              type="text"
+              placeholder="display name"
+              onChange={(e) => setDisplayName(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4"
+            />
+            <div className="flex justify-end">
+              <button
+                onClick={handleUpdate}
+                className="bg-primary hover:bg-primary/90 text-white px-6 py-2 rounded-md"
+              >
+                확인
+              </button>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="bg-red-500 hover:bg-red-500/90 text-white px-6 py-2 rounded-md ml-2"
+              >
+                닫기
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/libs/supabase-client.ts
+++ src/libs/supabase-client.ts
@@ -4,6 +4,11 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabaseSecret = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl!, supabaseKey!);
-const supabaseService = createClient(supabaseUrl!, supabaseSecret!);
+const supabaseService = createClient(supabaseUrl!, supabaseSecret!, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
 
 export { supabase, supabaseService };
